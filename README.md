ecd2d1b5cab3ceee65506731dcafbf90a687866c
vishwanath-kadam <vishwanath.kadam.in@gmail.com>
2025-05-24 16:16:52 +0530

    Added two to six examples.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 
 ## Playlist link
 Java 8 Streams API Interview Questions (Most Asked)
+
 https://www.youtube.com/watch?v=xifdVmHOAWc&list=PL63BDXJjNfTElajNCfg_2u_pbe1Xi7uTy
 
 ### 01 
-Given a sentence, find the word that has the highest length
+Find the word that has the highest length in the given sentence.
 ### 02
+Remove duplicates from the string.
+### 03
+Find the word that has the 2nd highest length in the given sentence.
+### 04
+Find the length of 2nd highest word in the given sentence.
+### 05
+Find the occurrence of each word in the given sentence.
+### 06
+Find the words with a specified(2) number of vowels in the given sentence.
--- src/main/java/com/vishwa/Main.java
+++ src/main/java/com/vishwa/Main.java
@@ -2,12 +2,71 @@ package com.vishwa;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         one();
+        two();
+        two_1();
+        three();
+        four();
+        five();
+        six();
+    }
+
+    private static void six() {
+        String intputString = "I am learning Stream API's in JAVA";
+        Arrays.stream(intputString.split(" "))
+                .filter(s->s.replaceAll("[^aeiouAEIOU]", "").length() == 2)
+                .forEach(System.out::println);
+    }
+
+    private static void five() {
+        String intputString = "I am learning Stream API's in JAVA";
+        Map<String, Long> wordCount = Arrays.stream(intputString.split(""))
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        System.out.println(wordCount);
+
+    }
+
+    private static void four() {
+        String intputString = "I am learning Stream API's in JAVA";
+        Integer secondHighestLength = Arrays.stream(intputString.split(" "))
+                .map(s->s.length())
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
+        System.out.println(secondHighestLength);
+    }
+
+    private static void three() {
+        String intputString = "I am learning Stream API's in JAVA";
+        String secondHighest = Arrays.stream(intputString.split(" "))
+                .sorted(Comparator.comparing(String::length).reversed())
+                .skip(1)
+                .findFirst()
+                .get();
+        System.out.println(secondHighest);
+    }
+
+    private static void two_1() {
+        String inputString = "dabcadefg";
+        Arrays.stream(inputString.split(""))
+                .distinct()
+                .forEach(System.out::print);
+    }
+
+    private static void two() {
+        String inputString = "dabcadefg";
+        inputString.chars()
+                .distinct()
+                .mapToObj(s->(char)s)
+                .forEach(System.out::print);
     }
 
     private static void one() {
